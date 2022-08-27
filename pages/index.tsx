e064cc26cf7d00f3ded9e0b7e0463f1755f48fd4
Chiaki Chen <chiaki@portto.io>
2022-08-28 02:02:34 +0800

    [Fix] Mobile style and background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import { Box, Center, keyframes } from '@chakra-ui/react'
-import { motion, useScroll } from 'framer-motion'
+import { motion } from 'framer-motion'
 import DotBackground from 'components/DotBackground'
 import AnimatedLogo from 'components/AnimatedLogo'
 import BgImg from '@img/takuzosu-inari-shrine.jpg'
 import LogoText from '@icon/logo_text_light.svg'
-import LogoIcon from '@icon/logo_white.svg'
 
 const spin = keyframes`
   from {transform: rotate(0deg);}
@@ -14,20 +13,37 @@ const spin = keyframes`
 const Home: NextPage = () => {
   return (
     <Box
+      backgroundColor="black"
       backgroundImage={BgImg.src}
       backgroundSize="cover"
       width="100vw"
       height="100vh"
     >
       <DotBackground />
-      <Center position="relative" width="100%" height="100%" justifyContent="center">
-        <Box animation={`${spin} infinite 15s linear`}>
+      <Center
+        position="relative"
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        flexDirection={{ base: 'column', md: 'row' }}
+      >
+        <Box
+          animation={`${spin} infinite 15s linear`}
+          width={{ base: '20vw', md: '8vw' }}
+        >
           <AnimatedLogo />
         </Box>
 
-        <LogoText style={{ width: '50%', maxWidth: '700px', margin: '0 20px' }} />
+        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+          <Box width={{ base: '85vw', md: '50vw' }} maxWidth="700px" m="30px 20px">
+            <LogoText style={{ width: '100%' }} />
+          </Box>
+        </motion.div>
 
-        <Box animation={`${spin} infinite 15s linear`}>
+        <Box
+          animation={`${spin} infinite 15s linear reverse`}
+          width={{ base: '20vw', md: '8vw' }}
+        >
           <AnimatedLogo />
         </Box>
       </Center>
