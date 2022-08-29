a05a69f7f29d2ab3835c3530a0d8b55709c08f3b
Chiaki.C <chiaki@portto.com>
2022-08-29 14:39:02 +0800

    feat: use shorter css var prefix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  config: {
+    cssVarPrefix: 'c',
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Head>
         <title>Chiaki Inari</title>
         <meta name="description" content="Chiaki Inari" />
