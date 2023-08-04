18ddbfb8c31fe1c554545e9ef89df232d5c8fb82
Chiaki.C <chiaki@portto.com>
2023-08-04 17:11:54 +0800

    refactor: detail section

--- components/index/Detail.tsx
+++ components/index/Detail.tsx
@@ -1,38 +1,8 @@
-import { useRef } from 'react'
-import { Box, Flex } from '@chakra-ui/react'
-import { motion, useScroll, useTransform, MotionValue } from 'framer-motion'
-import Image from 'next-image-export-optimizer'
-import { useParallax } from 'utils/hooks'
-
-const BlackoutText = ({
-  progress,
-  children,
-}: {
-  progress: MotionValue<string>
-  children: React.ReactNode
-}) => (
-  <Box position="relative" width="fit-content" my="10px">
-    <motion.div
-      style={{
-        backgroundColor: 'black',
-        position: 'absolute',
-        right: 0,
-        height: '100%',
-        width: progress,
-      }}
-    />
-    {children}
-  </Box>
-)
+import { Box, Grid, Heading } from '@chakra-ui/react'
 
 const Detail = () => {
-  const ref = useRef(null)
-  const { scrollYProgress } = useScroll({ target: ref })
-  const y = useParallax(scrollYProgress, 200)
-  const wrapperY = useTransform(scrollYProgress, [0, 0.6, 1], ['100%', '100%', '0%'])
-
   return (
-    <Box height="200vh" width="100%" ref={ref}>
+    <Box height="200vh" width="100%">
       <Box
         position="sticky"
         top="0"
@@ -44,51 +14,33 @@ const Detail = () => {
           height="100%"
           maxWidth="900px"
           marginX={{ base: '10px', md: '100px', lg: 'auto' }}
+          fontFamily="creamfont"
         >
-          <Flex height="100%" width="100%" alignItems="center" position="absolute">
-            <Box
-              width="calc(100vw - 20px)"
-              maxWidth="400px"
-              zIndex="10"
-              my="50px"
-              backgroundColor="rgba(255,255,255,0.7)"
-              alignSelf={{ base: 'flex-start', sm: 'center' }}
-            >
-              <BlackoutText progress={wrapperY}>
-                <Box fontWeight="900" fontSize="40px" fontFamily="creamfont">
-                  千秋のご挨拶
-                </Box>
-              </BlackoutText>
-              <BlackoutText progress={wrapperY}>
-                本神社之祭奉神明──稻荷大神，被尊崇為「掌管衣食住的根基，庇佑所有人生活富饒快樂的神明」。
-              </BlackoutText>
-              <BlackoutText progress={wrapperY}>
-                做為3萬稻荷神社之一，本社受理祈願五穀豐登、商業興盛、家庭安全、實現諸願望等各式委託，於網路的世界為大家服務。
-              </BlackoutText>
-              <BlackoutText progress={wrapperY}>
-                為了達成帶給大家快樂的目標，本社目前正在嘗試偶像活動，敬請多加支持！
-              </BlackoutText>
+          <Grid p="15px" gap="15px">
+            <Heading mt="40px">千秋稲荷社とは</Heading>
+            <Box mb="40px">
+              千秋稲荷社是涼風千秋創立的複合社團。同三萬稲荷神社一般，我們的願望與使命便是「庇佑所有人生活富饒快樂」。
+              <br />
+              本社受理祈願五穀豐登、商業興盛、家庭安全、實現諸願望等各式委託，於網路的世界為大家服務。
             </Box>
-          </Flex>
-
-          <Box
-            width="700px"
-            float="right"
-            marginRight={{
-              base: '-250px',
-              sm: '-200px',
-              lg: '-100px',
-            }}
-          >
-            <motion.div style={{ y }}>
-              <Image
-                src="/assets/img/shone.png"
-                alt="Chiaki"
-                width="1240px"
-                height="2000px"
-              />
-            </motion.div>
-          </Box>
+            <Heading mb="20px">社務</Heading>
+            <Heading size="lg">字體設計</Heading>
+            <Box>
+              我們有專業的字體設計經驗，無論是標準字設計、形象字型設計、字體選擇與字體排版諮詢服務，都能夠根據客戶需求，創作出符合品牌形象和風格的獨特字體，提高品牌識別度和形象。
+            </Box>
+            <Heading size="lg">網頁設計</Heading>
+            <Box>
+              我們提供網頁設計服務，包括視覺設計、網站架構、網站內容策略等方面，讓您的網站能夠更好地展示品牌形象和產品資訊。
+            </Box>
+            <Heading size="lg">插畫</Heading>
+            <Box>
+              接受插畫委託，包括全彩插圖、全身半身立繪、大頭貼、服裝設計與角色設計。
+            </Box>
+            <Heading size="lg">御守製作</Heading>
+            <Box>
+              我們提供客製化御守製作服務，將您想提供的圖片、文字等資訊傳給我們，我們會為您製作出獨一無二的御守，並在完成後寄送給您。
+            </Box>
+          </Grid>
         </Box>
       </Box>
     </Box>
