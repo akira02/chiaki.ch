8531c9bcded83f7fd27434d2cddc06da86efa05f
Chiaki.C <akiakira02@gmail.com>
2025-06-05 16:52:18 +0900

    fix: add metatags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,58 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="zh-TW">
         <Head>
+          {/* Basic meta tags */}
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="千秋稻荷社 - Chiaki Inari Shrine，千秋的個人網站與作品展示"
+          />
+          <meta name="keywords" content="千秋,稻荷社,Chiaki,個人網站,作品集" />
+          <meta name="author" content="千秋" />
+          <meta name="robots" content="index, follow" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://chiaki.ch/" />
+          <meta property="og:title" content="千秋稻荷社 - Chiaki Inari Shrine" />
+          <meta
+            property="og:description"
+            content="千秋稻荷社 - Chiaki Inari Shrine，千秋的個人網站與作品展示"
+          />
+          <meta property="og:image" content="https://chiaki.ch/thumbnail.jpeg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:site_name" content="千秋稻荷社" />
+          <meta property="og:locale" content="zh_TW" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://chiaki.ch/" />
+          <meta
+            property="twitter:title"
+            content="千秋稻荷社 - Chiaki Inari Shrine"
+          />
+          <meta
+            property="twitter:description"
+            content="千秋稻荷社 - Chiaki Inari Shrine，千秋的個人網站與作品展示"
+          />
+          <meta
+            property="twitter:image"
+            content="https://chiaki.ch/thumbnail.jpeg"
+          />
+
+          {/* Additional SEO meta tags */}
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="application-name" content="千秋稻荷社" />
+          <meta name="apple-mobile-web-app-title" content="千秋稻荷社" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+          <title>千秋稻荷社 - Chiaki Inari Shrine</title>
+
           <link rel="preconnect" href="https://s3-ap-northeast-1.amazonaws.com" />
           <script
             src="//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-64294.js"
@@ -33,8 +83,10 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#a25c01" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
-        <Main />
-        <NextScript />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
